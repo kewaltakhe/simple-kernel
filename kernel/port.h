99758d141aae8782005e74d09ca8e1e8c3615304
Kewal Takhellambam <43235441+kewaltakhe@users.noreply.github.com>
2023-08-26 10:25:23 +0530

    Delete kernel directory

--- kernel/port.h
+++
@@ -1,8 +0,0 @@
-#ifndef PORT_H
-#define PORT_H
-#include"../cpu/types.h"
-u8 port_byte_in(u16 port);
-void port_byte_out(u16 port, u8 data);
-u16 port_word_in(u16 port);
-void port_word_out(u16 port, u16 data);
-#endif
