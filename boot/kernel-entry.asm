65bd0cbc697631bec45f07d52d10cb81b918fbaf
Kewal Takhellambam <43235441+kewaltakhe@users.noreply.github.com>
2023-08-26 10:24:48 +0530

    Delete boot directory

--- boot/kernel-entry.asm
+++
@@ -1,4 +0,0 @@
-[bits 32]       ; now we have entered the protected in PM mode. Use 32-bit instruction 
-[extern main]   ; extern declares a symbol the linker must search in other object files
-call main       ; call our kernel_entry function, 'main', an external symbol
-jmp $           ; hang forever when we return from the kernel
